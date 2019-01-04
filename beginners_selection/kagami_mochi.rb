4781f2423baeb05fd6a4629b7fff134bbc91068e
ryu022304 <ryu022304@gmail.com>
2019-01-04 15:58:56 +0900

    [update]集合での実装を追記

--- beginners_selection/kagami_mochi.rb
+++ beginners_selection/kagami_mochi.rb
@@ -1,8 +1,17 @@
+require 'set'
 n = gets.chomp.to_i
 
-mochi_list = []
+mochi_set = Set.new
+
 n.times do
-  mochi_list << gets.chomp.to_i
+  mochi_set.add(gets.chomp.to_i)
 end
 
-puts mochi_list.uniq.size
+# 集合よりも重複を後で消す方が早かった
+#mochi_list = []
+#n.times do
+#  mochi_list << gets.chomp.to_i
+#end
+
+#puts mochi_list.uniq.size
+puts mochi_set.size
