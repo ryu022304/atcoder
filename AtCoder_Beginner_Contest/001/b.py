b94e63f61ced5dc482ba35a6247fff88f6d73237
ryu022304 <ryu022304@gmail.com>
2019-01-23 19:56:50 +0900

    [fix]未満と以下を間違えていた

--- AtCoder_Beginner_Contest/001/b.py
+++ AtCoder_Beginner_Contest/001/b.py
@@ -1,12 +1,12 @@
 m = int(input())
 
-if m <= 100:
+if m < 100:
     print("{0:02d}".format(0))
-elif 100 <= m and m <= 5000:
+elif 100 <= m <= 5000:
     print("{0:02d}".format(int(m/1000*10)))
-elif 6000 <= m and m <= 30000:
+elif 6000 <= m <= 30000:
     print("{0:02d}".format(int(m/1000)+50))
-elif 35000 <= m and m <= 70000:
+elif 35000 <= m <= 70000:
     res = int((m/1000-30)/5+80)
     print("{0:02d}".format(res))
 elif 70000 < m:
