68d89202a674a4ba8a87e8ab27509802fc89bd47
ryu022304 <ryu022304@gmail.com>
2019-01-28 22:21:35 +0900

    [update]解答例追記

--- NIKKEI_Programming_Contest_2019/b_touitsu.py
+++ NIKKEI_Programming_Contest_2019/b_touitsu.py
@@ -1,3 +1,4 @@
+'''
 n = int(input())
 a = list(input())
 b = list(input())
@@ -13,4 +14,13 @@ for i in range(n):
     else:
         count += 2
 
+print(count)
+
+'''
+n = int(input())
+a,b,c = input(),input(),input()
+
+count = 0
+for i in range(n):
+    count += len(set([a[i],b[i],c[i]]))-1
 print(count)
