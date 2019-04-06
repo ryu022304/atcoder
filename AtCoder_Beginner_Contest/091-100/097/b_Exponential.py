27f0310af4402a389431cb6298b979b2a967fdf9
ryu022304 <ryu022304@gmail.com>
2019-04-07 02:12:50 +0900

    [add]097,b

--- AtCoder_Beginner_Contest/091-100/097/b_Exponential.py
+++ AtCoder_Beginner_Contest/091-100/097/b_Exponential.py
@@ -1,6 +1,9 @@
 x = int(input())
-res_list = []
-for i in range(1,x+1):
-    if (i**0.5).is_integer():
-        res_list.append(i)
+res_list = [1]
+for b in range(1,x+1):
+    for p in range(2,x+1):
+        if b**p <= x:
+            res_list.append(b**p)
+        else:
+            break
 print(max(res_list))
