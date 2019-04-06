bcd874d9a9f0839f0930e70c4b640dc5b2497883
ryu022304 <ryu022304@gmail.com>
2019-04-07 01:52:15 +0900

    [add]054,b

--- AtCoder_Beginner_Contest/051-060/054/b_Template_Matching.py
+++ AtCoder_Beginner_Contest/051-060/054/b_Template_Matching.py
@@ -1,9 +1,17 @@
 n,m = map(int,input().split())
 a_list = [input() for _ in range(n)]
 b_list = [input() for _ in range(m)]
-count = 0
-print(a_list)
-print(b_list)
-for i,b in enumerate(b_list):
-    for j,a in enumerate(a_list):
-        
+for i in range(n-m+1):
+    for j in range(n-m+1):
+        f = True
+        for k in range(m):
+            for l in range(m):
+                if a_list[i+k][j+l] != b_list[k][l]:
+                    f = False
+                    break
+            if not f:
+                break
+        if f:
+            print('Yes')
+            exit()
+print('No')
