331c5907d96958d4540e9df589324731c4d4ac7a
ryu022304 <ryu022304@gmail.com>
2019-04-14 01:44:50 +0900

    [update]124,c

--- AtCoder_Beginner_Contest/124/c_Coloring_Colorfully.py
+++ AtCoder_Beginner_Contest/124/c_Coloring_Colorfully.py
@@ -1,10 +1,8 @@
-import math
-s = input()
-zero_one = s.count('01')
-one_zero = s.count('10')
-zero_zero = s.count('00')
-one_one = s.count('11')
-base = len(s)//2
+s = list(input())
+one_zero_list = ['0' if i%2==0 else '1' for i in range(len(s))]
+zero_one_list = ['1' if i%2==0 else '0' for i in range(len(s))]
 
-if one_zero==base and zero_one==base:
-    print(0)
+count_one_zero = len([0 for i in range(len(s)) if s[i]!=one_zero_list[i]])
+count_zero_one = len([0 for i in range(len(s)) if s[i]!=zero_one_list[i]])
+
+print(min(count_one_zero,count_zero_one))
