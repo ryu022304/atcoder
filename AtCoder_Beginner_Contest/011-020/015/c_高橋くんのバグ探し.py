609497d223c5a244ae8ed2a9f6283f0e0325ef19
ryu022304 <ryu022304@gmail.com>
2019-05-04 12:03:44 +0900

    [update]itertools.productでの解き方も追記

--- AtCoder_Beginner_Contest/011-020/015/c_高橋くんのバグ探し.py
+++ AtCoder_Beginner_Contest/011-020/015/c_高橋くんのバグ探し.py
@@ -8,5 +8,17 @@ def dfs(question, value):
         if dfs(question+1, value^t_list[question][i]):
             return True
     return False
-    
-print('Found' if dfs(0,0) else 'Nothing')
+
+#print('Found' if dfs(0,0) else 'Nothing')
+import itertools
+f = False
+print(*t_list)
+for t in itertools.product(*t_list):
+    print(t)
+    tmp = 0
+    for v in t:
+        tmp = tmp^v
+    if tmp == 0:
+        f = True
+        break
+print('Found' if f else 'Nothing')
