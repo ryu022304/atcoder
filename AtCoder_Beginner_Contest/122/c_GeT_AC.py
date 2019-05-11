8592a45dd6a9d5f7796d4b13daa8a0b7c9ac5156
ryu022304 <ryu022304@gmail.com>
2019-05-11 21:06:07 +0900

    [add]122,c

--- AtCoder_Beginner_Contest/122/c_GeT_AC.py
+++ AtCoder_Beginner_Contest/122/c_GeT_AC.py
@@ -1,10 +1,13 @@
 n,q = map(int, input().split())
 s = list(input())
 lr_list = [list(map(int,input().split())) for _ in range(q)]
+tmp_list = [0]*(n+1)
 
-ac_list = [1 if ''.join(s[i:i+2]) == 'AC' else 0 for i in range(len(s))]
-print(ac_list)
+for i in range(1,n):
+    if s[i-1]=='A' and s[i]=='C':
+        tmp_list[i] = tmp_list[i-1]+1
+    else:
+        tmp_list[i] = tmp_list[i-1]
+
 for l,r in lr_list:
-    #tmp_str = ''.join(s[l-1:r])
-    #print(tmp_str.count('AC'))
-    print(sum(ac_list[l-1:r-1]))
+    print(tmp_list[r-1]-tmp_list[l-1])
