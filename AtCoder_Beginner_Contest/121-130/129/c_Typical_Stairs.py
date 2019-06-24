fb082325fa3e48293a5347d40f5e7a54e5ca1a17
ryu022304 <ryu022304@gmail.com>
2019-06-24 22:33:33 +0900

    [add]129,c

--- AtCoder_Beginner_Contest/121-130/129/c_Typical_Stairs.py
+++ AtCoder_Beginner_Contest/121-130/129/c_Typical_Stairs.py
@@ -1,22 +1,13 @@
-import sys
-sys.setrecursionlimit(2000)
-
 n,m = map(int,input().split())
 a_list = [int(input()) for _ in range(m)]
-count = 0
-dp = [10**6]*n
-def dfs(pos):
-    global count
-    print(pos, count)
-    if pos >= n-1:
-        count += 1
-        return True
-    for i in [1,2]:
-        if pos+i in a_list:
-            pass
-        else:
-            dfs(pos+i)
-    return True
 
-dfs(0)
-print(count%1000000007)
+dp = [1]*(n+1)
+
+for a in a_list:
+    dp[a] = 0
+
+for i in range(2,n+1):
+    if dp[i] != 0:
+        dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n]%(10**9+7))
