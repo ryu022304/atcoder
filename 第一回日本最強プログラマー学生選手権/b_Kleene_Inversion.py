f8736a2a1234805e9a1d541fdd9afa90fa911e18
ryu022304 <ryu022304@gmail.com>
2019-08-24 23:17:16 +0900

    [update]b修正

--- 第一回日本最強プログラマー学生選手権/b_Kleene_Inversion.py
+++ 第一回日本最強プログラマー学生選手権/b_Kleene_Inversion.py
@@ -1,9 +1,17 @@
 n,k = map(int,input().split())
 a_list = list(map(int,input().split()))
-a_list_k = a_list*k
 count = 0
-for i in range(len(a_list_k)):
-    for j in range(i,len(a_list_k)):
-        if a_list_k[i] > a_list_k[j]:
+for i in range(n):
+    for j in range(i,n):
+        if a_list[i] > a_list[j]:
             count += 1
-print(count)
+
+tmp_list = [0]*n
+for i in range(n):
+    cnt = 0
+    for a in a_list:
+        if a_list[i] > a:
+            cnt += 1
+    tmp_list[i] = cnt
+res = k * (2*count + sum(tmp_list) * (k-1)) // 2 
+print(res % (10**9+7))
