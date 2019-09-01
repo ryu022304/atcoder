ba419450202e8b03a73341bd744738021cced58c
ryu022304 <ryu022304@gmail.com>
2019-09-01 22:59:26 +0900

    [update]139,b,c

--- AtCoder_Beginner_Contest/139/c_Lower.py
+++ AtCoder_Beginner_Contest/139/c_Lower.py
@@ -1,15 +1,12 @@
 n = int(input())
 h_list = list(map(int,input().split()))
+tmp_list = []
 count = 0
-max_count = 0
-max_num = 0
-for h in h_list[::-1]:
-    if max_num <= h:
+for i in range(len(h_list)-1):
+    if h_list[i] >= h_list[i+1]:
         count += 1
-        max_num = max(max_num, h)
     else:
-        max_count = max(max_count, count)
-        count = 1
-        max_num = 0
-max_count = max(max_count, count)
-print(max_count-1)
+        tmp_list.append(count)
+        count = 0
+tmp_list.append(count)
+print(max(tmp_list))
